9380ded155103e95842f922736f87567b93ece52
SuvuYou <nazarsuvyliak1@gmail.com>
2024-10-17 21:23:44 +0300

    always use warping

--- Assets/_Scripts/WorldGeneration/Biomes/Biome.cs
+++ Assets/_Scripts/WorldGeneration/Biomes/Biome.cs
@@ -5,21 +5,13 @@ public class Biome : MonoBehaviour
     [SerializeField] private NoiseSettings _biomeNoiseSettings;
     [SerializeField] private DomainWarping _domainWarping;
     [SerializeField] private TerrainBlockSelector _terrainBlockSelector;
-    [SerializeField] private bool UseWarping = false;
 
     public int GetGroundLevel(int x, int z, float chunkHeight, Vector2Int seedMapOffset)
     {
         float noiseValue;
 
-        if (UseWarping)
-        {
-            noiseValue = _domainWarping.GetNoiseValue(x, z, _biomeNoiseSettings, seedMapOffset);
-        }
-        else
-        {
-            noiseValue = CustomNoise.OctavePerlin(x, z, _biomeNoiseSettings, seedMapOffset);
-        }
-        
+        noiseValue = _domainWarping.GetNoiseValue(x, z, _biomeNoiseSettings, seedMapOffset);
+
         noiseValue = CustomNoise.NoiseRedistribution(noiseValue, _biomeNoiseSettings);
 
         return (int)CustomNoise.RemapValueFromNormalRange(noiseValue, 0, chunkHeight);
